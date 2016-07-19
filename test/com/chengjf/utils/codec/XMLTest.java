34720c211d166eff87cc13190f91365ffa172459
chengjf <sharpkingdom@gmail.com>
2016-07-19 17:11:57 +0800

    使用javax.xml下的包，而非显示的使用sun的包。

--- test/com/chengjf/utils/codec/XMLTest.java
+++ test/com/chengjf/utils/codec/XMLTest.java
@@ -3,10 +3,12 @@ package com.chengjf.utils.codec;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
-import com.sun.org.apache.xerces.internal.parsers.SAXParser;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * XML相关操作
@@ -22,19 +24,22 @@ public class XMLTest {
 
 	/**
 	 * 校验XML字符串格式是否正确
-	 * 
 	 */
 	private static void validateXMLFormat() {
 		String xmlStr = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><insert>a.the_date <= sysdate - 90</insert>";
-		
-		SAXParser parser = new SAXParser();
+
 		try {
-			ByteArrayInputStream inputStream = new ByteArrayInputStream(xmlStr.getBytes());
-			parser.parse(new InputSource(inputStream));
+			SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(
+					xmlStr.getBytes());
+			parser.parse(inputStream, new DefaultHandler());
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
