d42f86de922cd95627cb58bbf76bd1303cc613c6
chengjf <sharpkingdom@gmail.com>
2016-08-25 13:36:04 +0800

    XMLTest加入DTD的校验。

--- test/com/chengjf/utils/codec/XMLTest.java
+++ test/com/chengjf/utils/codec/XMLTest.java
@@ -1,13 +1,29 @@
 package com.chengjf.utils.codec;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Stack;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.w3c.dom.Document;
+import org.xml.sax.Attributes;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.Locator;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -20,6 +36,8 @@ public class XMLTest {
 
 	public static void main(String[] args) {
 		validateXMLFormat();
+		XMLTest test = new XMLTest();
+		test.validateWithDTD("D:\\Users\\chengjianfeng100\\Desktop\\sqlmap-mapping-business.xml");
 	}
 
 	/**
@@ -42,6 +60,116 @@ public class XMLTest {
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 校验XML字符串格式是否正确
+	 */
+	private static void validateXMLFormat(String fileName) {
+
+		try {
+			SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+			InputStream inputStream = new FileInputStream(new File(fileName));
+			parser.parse(inputStream, new DefaultHandler());
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 使用DTD校验
+	 * resource目录下有DTD文件
+	 * 
+	 * @param fileName
+	 */
+	private void validateWithDTD(String fileName) {
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		spf.setValidating(true);
+		SAXParser saxParser;
+
+		try {
+			saxParser = spf.newSAXParser();
+			XMLReader xmlReader = saxParser.getXMLReader();
+
+			xmlReader.setEntityResolver(new ClasspathEntityResolver());
+
+			xmlReader.setDTDHandler(new DTDHandler() {
+
+				@Override
+				public void unparsedEntityDecl(String name, String publicId,
+						String systemId, String notationName)
+						throws SAXException {
+					// TODO Auto-generated method stub
+					System.out.println(name + publicId + systemId
+							+ notationName);
+				}
+
+				@Override
+				public void notationDecl(String name, String publicId,
+						String systemId) throws SAXException {
+					// TODO Auto-generated method stub
+					System.out.println(name + publicId + systemId);
+				}
+			});
+
+			xmlReader.setEntityResolver(new ClasspathEntityResolver());
+
+			xmlReader.setErrorHandler(new MyHandler());
+
+			xmlReader.parse(new InputSource(fileName));
+
+		} catch (ParserConfigurationException | SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	class MyHandler extends DefaultHandler {
+		String errorMessage = null;
+
+		public void error(SAXParseException e) throws SAXException {
+			errorMessage = e.getMessage();
+			System.out.println("一般错误：" + errorMessage);
+		}
+
+		public void fatalError(SAXParseException e) throws SAXException {
+			errorMessage = e.getMessage();
+			System.out.println("致命错误：" + errorMessage);
+		}
+	}
+
+	public class ClasspathEntityResolver implements EntityResolver {
+
+		public InputSource resolveEntity(String publicId, String systemId)
+				throws SAXException, IOException {
+
+			if (systemId != null) {
+
+				int index = systemId.lastIndexOf('/');
+				if (index != -1) {
+					systemId = systemId.substring(index + 1);
+				}
+				// systemId = "/" + systemId;
+				System.out.println(systemId);
+				InputStream istr = Thread.currentThread()
+						.getContextClassLoader().getResourceAsStream(systemId);
+				if (istr != null) {
+					return new InputSource(istr);
+				}
+			}
+			return null;
 		}
 	}
 }
